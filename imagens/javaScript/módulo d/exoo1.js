2ad5d20e9f2d66d4d52bc5ff696b09a6e67a215e
Thalestata <118277842+Thalestata@users.noreply.github.com>
2022-11-30 15:43:28 -0300

    atualização

---
+++ imagens/javaScript/módulo d/exoo1.js
@@ -0,0 +1,6 @@
+var vel =70
+console.log(`A velocidade do seu caro é: ${vel}km/h`)
+    if (vel > 60){
+        console.log('voce ultrapassou a velocidade permitida. MULTADO')
+    }
+console.log('Digija sempre com sinto de segurança')
